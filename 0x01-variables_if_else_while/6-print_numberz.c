e5ebe6f672c9c58579cf4ca9d50f67547631e16f
omarbdw <omarbdw@yahoo.com>
2023-08-13 06:00:29 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 /**
-* main - Prints the numbers.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char num[10] = "0123456789";
 int i;
-for (i = 0; i < 10; i++)
+for (i = 48; i < 58; i++)
 {
-putchar(num[i]);
+putchar(i);
 }
 putchar('\n');
 return (0);
