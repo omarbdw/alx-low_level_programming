fa3c1fbfe5db85c7cfeaa6661a51ebdda029742a
omarbdw <omarbdw@yahoo.com>
2023-08-13 03:09:24 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alph[i]);
+}
+putchar('\n');
+return (0);
+}
