14a05bfbd445d2594a00f7157bd1646985b4800c
omarbdw <omarbdw@yahoo.com>
2023-08-13 05:50:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the numbers.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char num[10] = "0123456789";
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(num[i]);
+}
+putchar('\n');
+return (0);
+}
