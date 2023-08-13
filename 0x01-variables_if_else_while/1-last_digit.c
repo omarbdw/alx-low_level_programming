7efa5c00f2c0e9734ef746e017c52af4777e06f0
omarbdw <omarbdw@yahoo.com>
2023-08-13 02:30:24 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,5 +18,5 @@ else if (lastDigit == 0)
 printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 else if (lastDigit < 6 && n != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-return(0);
+return (0);
 }
