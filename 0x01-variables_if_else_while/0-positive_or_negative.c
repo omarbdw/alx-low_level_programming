66ba23cfa554aff9b0137391c08e9a8d71d4e725
omarbdw <omarbdw@yahoo.com>
2023-08-13 00:26:03 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*Decides weather random n number is positive, negative, or zero*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n>0)
+    {
+    printf("%d is positive\n", n);
+    }
+  else if (n<0)
+    {
+    printf("%d is negative\n", n);
+    }
+    else if (n==0)
+      {
+      printf("%d is zero\n", n);
+      }
+      return (0);
+}
