ad4889990b0e2884dbb833421a42d56fed413ef5
omarbdw <omarbdw@yahoo.com>
2023-08-17 09:51:56 -0700

    1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - checks for uppercase character
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+int _isupper(char c)
+{
+  char c;
+  scanf("", c);
+  if ('A' >= c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+  return (0);
+    }
+    }
