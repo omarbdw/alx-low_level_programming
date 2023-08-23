771096c3efd9fd3fe07c11966c6c9d8396d945fd
omarbdw <omarbdw@yahoo.com>
2023-08-23 01:37:26 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int i;
-  int j;
-  i = 0;
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  j = 0;
-  while (src[j] != '\0')
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  return (dest);
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return (dest);
 }
