025b7108680dfcf4565aa35c05fbf15d132ec2ba
omarbdw <omarbdw@yahoo.com>
2023-09-04 12:44:10 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,26 +13,31 @@ char *str_concat(char *s1, char *s2)
 int len1 = 0;
 int  len2 = 0;
 int i = 0;
+ int j = 0;
 char *resultString;
-while (s1[len1] != '\0')
+ if (s1 == NULL)
+   return (NULL);
+ if (s2 == NULL)
+   return (NULL);
+ while (s1[len1] != '\0')
 len1++;
 while (s2[len2] != '\0')
 len2++;
-resultString = malloc(sizeof(char) * (len1 + len2 + 2));
+resultString = malloc(sizeof(char) * (len1 + len2 + 1));
 
-while (s1[i])
+while (s1[i] != '0')
 {
 resultString[i] = s1[i];
 i++;
 }
-i++;
 
-while (i >= len1 || i <= (len1 + len2))
+while (s2[j] != '\0')
 {
-resultString[i] = s2[i];
+resultString[i] = s2[j];
 i++;
+j++;
 }
-resultString[len1 + len2 + 1] = '\0';
+resultString[j] = '\0';
 return (resultString);
 free(resultString);
 }
