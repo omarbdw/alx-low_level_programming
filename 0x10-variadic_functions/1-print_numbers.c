5437c65e8317ed23a7c6586baeea2c6ecb9888c1
omarbdw <omarbdw@yahoo.com>
2023-09-14 22:45:01 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,6 +6,7 @@
  * print_numbers - prints numbers, followed by a new line
  * @separator: sign between words
  * @n: number of arguments
+ * @...: A variable number of numbers to be printed
  * Return: nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
