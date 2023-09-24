77c0b434639ff05d0d9bdd97c5745262f7a1a209
omarbdw <76036118+omarbdw@users.noreply.github.com>
2023-09-24 11:54:56 +0200

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <string.h>
-/* add_node - adds a new node at the beginning of a list_t list
+/* 
+* add_node - adds a new node at the beginning of a list_t list
     * @head: pointer to the head of the list
     * @str: string to be added to the list
     * Return: address of the new element or NULL if it failed
@@ -9,15 +10,15 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-int i;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-for (i = 0; str[i] != '\0'; i++);
-new_node->len = i;
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
+list_t *new_node; /* create a new node */
+int i; /* counter for the number of characters */
+new_node = malloc(sizeof(list_t)); /* allocate memory for the new node */
+if (new_node == NULL) /* validate memory allocation */
+return (NULL); /* return NULL if malloc fails */
+new_node->str = strdup(str); /* copy the string into the new node */
+for (i = 0; str[i] != '\0'; i++); /* count the number of characters */
+new_node->len = i; /* i is the number of characters */
+new_node->next = *head; /* the new node points to the old head */
+*head = new_node; /* the new node is now the head */
+return (new_node); /* return the new node */
+} 
