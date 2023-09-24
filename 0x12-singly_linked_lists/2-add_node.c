9d384022979ea3884c46accd823c4bf3f0c1faea
omarbdw <76036118+omarbdw@users.noreply.github.com>
2023-09-24 12:52:03 +0200

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <string.h>
-/*
-* add_node - adds a new node at the beginning of a list_t list
+/**
+    * *add_node - adds a new node at the beginning of a list_t list
     * @head: pointer to the head of the list
     * @str: string to be added to the list
     * Return: address of the new element or NULL if it failed
