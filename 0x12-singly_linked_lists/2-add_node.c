e59e566e8170da7edc70a332c2108c7d090aa329
omarbdw <76036118+omarbdw@users.noreply.github.com>
2023-09-24 11:04:15 +0200

    adding descriptions

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,6 +7,22 @@
 * str needs to be duplicated
 * You are allowed to use strdup
 */
+/*
+* 1. Create a new node
+* 2. Set the new node's str to the string passed in
+* 3. Set the new node's len to the length of the string
+* 4. Set the new node's next to the current head
+* 5. Set the head to the new node
+* 6. Return the new node
+*/
+ /*
+ * add_node - adds a new node at the beginning of a list_t list
+    * @head: pointer to the head of the list
+    * @str: string to be added to the list
+    * Return: address of the new element or NULL if it failed
+    * Description: adds a new node at the beginning of a list_t list
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
