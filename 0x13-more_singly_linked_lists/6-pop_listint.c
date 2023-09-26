5c51ffbd7791fd0fdbed5c9980f3031152cc3923
root <root@DESKTOP-VR3FSHG.localdomain>
2023-09-26 22:30:04 +0300

    six

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,9 +8,10 @@
 int pop_listint(listint_t **head)
 {
 listint_t *temp;
-int value = (*head)->n;
+int value;
 if (*head == NULL || head == NULL)
 return (0);
+value = (*head)->n;
 temp = (*head)->next;
 free(*head);
 *head = temp;
