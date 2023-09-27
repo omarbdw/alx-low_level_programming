beb0cb093c124c36a97c6605718da40b3211d8be
omarbdw <omarbdw@yahoo.com>
2023-09-27 16:33:53 +0300

    NULL test

--- 0x13-more_singly_linked_lists/9-insert_nodeint copy.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint copy.c
@@ -1,40 +1,40 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a node node at a given position
  * @head: head of list
- * @idx: index of the list where the new node should be added
+ * @idx: index of the list where the node node should be added
  * @n: value added
- * Return: the address of the new node, or NULL if it failed
- * if it is not possible to add the new node at index
- * idx, do not add the new node and return NULL
+ * Return: the address of the node node, or NULL if it failed
+ * if it is not possible to add the node node at index
+ * idx, do not add the node node and return NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-	listint_t *new;
+    unsigned int counter;
+	listint_t *node;
 	listint_t *temp = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	node = malloc(sizeof(listint_t));
+	if (!node || !head)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	node->n = n;
+	node->next = NULL; ///////////
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		node->next = *head;
+		*head = node;
+		return (node);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (counter = 0; temp && counter < idx; counter++)
 	{
-		if (i == idx - 1)
+		if (counter == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
 		}
 		else
 			temp = temp->next;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         {
             node->next = temp->next;
             temp->next = node;
-            return (temp);
+            return (node);
         }
         temp = temp->next;
         counter++;
