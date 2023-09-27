c6c221ad43fc5a5cc5d1ce989fa11587706d9af8
root <root@DESKTOP-VR3FSHG.localdomain>
2023-09-27 23:27:03 +0300

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,33 +1,35 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * delete_nodeint_at_index - deletes certain node
+ * @head: double pointer to head
+ * @index: index at which node will be deleted
+ * Return: 1 if ok & -1 if failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    unsigned int counter = 0;
-    listint_t *node;
-    listint_t *temp = malloc(sizeof(listint_t));
-    temp = *head;
-    if (!temp)
-        return (-1);
 
-    while (temp)
-    {
-        if (index == counter)
-        {
-            
-            temp = node->next;
-            free(node);
-            return (1);
-        }
-        else
-        {
-            temp = temp->next;
-            counter++;
-        }
-    }
-    return (-1);
+unsigned int position = 0;
+listint_t *temp = NULL;
+listint_t *current = *head;
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+*head = current->next;
+free(current);
+current = NULL;
+}
+while (position < (index - 1))
+{
+if (current == NULL || (current->next) == NULL)
+return (-1);
+current = current->next;
+position++;
+}
+
+temp = current->next;
+current->next = temp->next;
+free(temp);
+temp = NULL;
+return (1);
 }
