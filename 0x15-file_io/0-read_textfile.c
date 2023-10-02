0be5bf5125b9048b875f54a6da84c29d91744c60
Omarbdw <omarbdw@yahoo.com>
2023-10-02 22:12:15 +0300

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,27 @@
 #include "main.h"
+
 /**
- * read_textfile - Reads a text file
- * and prints it to the POSIX
- * @filename: The name of the file to read.
- * @letters: The number of letters to read from the file.
- * Return: The actual number of letters read,
- * 0 if cannot be opened or read.
+ * read_textfile- reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: name of file to be read
+ * @letters: number of bytes to read and print
+ * Return: number of bytes read and printed, 0 if error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t bytes_read;
-FILE *fp;
 char *buffer;
-/** Check if filename is NULL*/
-if (filename == NULL)
+ssize_t file;
+ssize_t wr;
+ssize_t temp;
+
+file = open(filename, O_RDONLY);
+if (file == -1)
 return (0);
-/** Open the file for reading */
-fp = fopen(filename, "r");
-if (fp == NULL)
-return (0);
-buffer = malloc(letters + 1);
-if (buffer == NULL)
-{
-fclose(fp);
-return (0);
-}
-bytes_read = fread(buffer, 1, letters, fp);
-if (bytes_read < 0)
-{
+buffer = malloc(sizeof(char) * letters);
+temp = read(file, buffer, letters);
+wr = write(STDOUT_FILENO, buffer, temp);
+
 free(buffer);
-fclose(fp);
-return (0);
-}
-buffer[bytes_read] = '\0';
-printf("%s", buffer);
-free(buffer);
-fclose(fp);
-return (bytes_read);
+close(file);
+return (wr);
 }
