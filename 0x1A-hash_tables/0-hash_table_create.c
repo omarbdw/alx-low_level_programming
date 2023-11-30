d166302aef2fd49016d5d07ba96d9f8d34d2ae10
omarbdw <omarbdw@yahoo.com>
2023-11-30 09:22:06 +0200

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *new_table = NULL;
+hash_node_t **new_array = NULL;
+unsigned long int i = 0;
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
+new_array = malloc(sizeof(hash_node_t *) * size);
+if (new_array == NULL)
+{
+free(new_table);
+return (NULL);
+}
+while (i < size)
+{
+new_array[i] = NULL;
+i++;
+}
+new_table->size = size;
+new_table->array = new_array;
+return (new_table);
+}
