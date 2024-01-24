298261be4872001b0b233da07fbc4fd9a0c88e5b
omarbdw <omarbdw@yahoo.com>
2024-01-24 11:54:06 +0200

    all

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (list): A list of lists representing the grid.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
+    return perimeter
